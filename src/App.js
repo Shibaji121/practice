fa4f86b962e5a48e75988361fe29f0da3c3c865b
shiba_1234 <shibajisahu121@gmail.com>
2023-06-05 17:50:22 +0530

    practice -2 -useMemo

--- src/App.js
+++ src/App.js
@@ -1,47 +1,23 @@
-import { useEffect, useRef, useState } from "react";
+import { useMemo, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [minute, setMinute] = useState(0);
-  const [second, setSecond] = useState(50);
-  const intervalref = useRef(null);
+  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    if (second === 59) {
-      setMinute((prev) => prev + 1);
-      setSecond(0);
-    }
-  }, [second]);
+  const doubledCount = useMemo(() => {
+    console.log("Expensive computation");
+    return count * 2;
+  }, [count]);
 
-  const handleStart = () => {
-    intervalref.current = setInterval(() => {
-      setSecond((prev) => prev + 1);
-    }, 1000);
-  };
-
-  const handlePause = () => {
-    clearInterval(intervalref.current);
-  };
-
-  const handleReset = () => {
-    setSecond(0);
-    setMinute(0);
-    clearInterval(intervalref.current);
+  const incrementCount = () => {
+    setCount(count + 1);
   };
 
   return (
-    <div className="App">
-      <h1>Shibaji</h1>
-      <div className="timer">
-        <span className="minute">{minute.toString().padStart(2, "0")}</span>
-        <span>:</span>
-        <span className="second">{second.toString().padStart(2, "0")}</span>
-      </div>
-      <div className="buttons">
-        <button onClick={handleStart}>Start</button>
-        <button onClick={handlePause}>Pause</button>
-        <button onClick={handleReset}>Reset</button>
-      </div>
+    <div>
+      <p>Count: {count}</p>
+      <p>Doubled Count: {doubledCount}</p>
+      <button onClick={incrementCount}>Increment</button>
     </div>
   );
 }
