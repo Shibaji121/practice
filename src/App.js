a4ef9930f417efe5d9a83f8f76ae677da967441f
shiba_1234 <shibajisahu121@gmail.com>
2023-06-05 17:23:20 +0530

    practice - 1 - stopwatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [minute, setMinute] = useState(0);
+  const [second, setSecond] = useState(50);
+  const intervalref = useRef(null);
+
+  useEffect(() => {
+    if (second === 59) {
+      setMinute((prev) => prev + 1);
+      setSecond(0);
+    }
+  }, [second]);
+
+  const handleStart = () => {
+    intervalref.current = setInterval(() => {
+      setSecond((prev) => prev + 1);
+    }, 1000);
+  };
+
+  const handlePause = () => {
+    clearInterval(intervalref.current);
+  };
+
+  const handleReset = () => {
+    setSecond(0);
+    setMinute(0);
+    clearInterval(intervalref.current);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shibaji</h1>
+      <div className="timer">
+        <span className="minute">{minute.toString().padStart(2, "0")}</span>
+        <span>:</span>
+        <span className="second">{second.toString().padStart(2, "0")}</span>
+      </div>
+      <div className="buttons">
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handlePause}>Pause</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
   );
 }
